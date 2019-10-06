76da5065aa23c49672628bb3cb9ca33a3b0fea99
Ayush Agrawal <nitaandroid121@gmail.com>
2019-10-06 10:38:42 +0530

    Set up basic folder structure and plan

---
+++ fpc.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+########################################################
+# This is main file which we use to create basic folder
+# structure for flask.
+
+# While creatind any folder structure we consider few things
+#  1> New_project
+#  2> model add
+#  3> controller add
+#  4> template add
+
+# This all file are added in base folder
+
+# There is one more folder plugin in which we
+# take care for flask plugin but not now
+
+########################################################
+
+import os
+import sys
+import logging
+import argparse 
+
+
+
+if __name__ == "__main__":
+    pass
