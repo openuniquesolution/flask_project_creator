dc34c78c1c3a77bf9fced5ba13465ac21a6a3108
Ayush Agrawal <nitaandroid121@gmail.com>
2019-10-08 14:23:38 +0530

    Add more file in new project

--- base/new_project.sh
+++ base/new_project.sh
@@ -6,6 +6,18 @@ basedir=`dirname $0`
 if [ ${basedir} == "." ]; then
 basedir=`pwd`
 fi
-cp ${basedir}/../templates/new_project/base_file ${name}.py
-mkdir -p ${name}/templates
-mkdir -p ${name}/static
+
+tem_dir="${basedir}/../templates/new_project"
+
+mkdir app
+mkdir -p app/templates
+mkdir -p app/static
+touch config.py
+cp ${tem_dir}/base_file ${name}.py
+cp ${tem_dir}/__init__ app/__init__.py
+
+###########################################
+# dot file creation
+
+touch .fpc
+cp ${tem_dir}/gitignore .gitignore
